d6da846a7b461707dddcb0c87e09b7028988cbc0
Cooper Oneida <conemusicproductions@gmail.com>
2025-02-24 11:35:00 -0700

    move torch serialization out of block
    oops

--- train.py
+++ train.py
@@ -12,16 +12,16 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--checkpoint", action="store", help="Checkpoint file to resume from")
     args = parser.parse_args()
 
+    # the newest ver of torch does not like some of the DP stuff
+    torch.serialization.add_safe_globals([Preprocessor])
+    torch.serialization.add_safe_globals([LanguageTokenizer])
+    torch.serialization.add_safe_globals([SequenceTokenizer])
+
     num_gpus = torch.cuda.device_count()
     config_file = os.path.join("experiments", args.exp_name, "config.yaml")
 
     if args.checkpoint is not None:
         print(f"Resuming training from checkpoint {os.path.basename(args.checkpoint)}\n")
-
-        # the newest ver of torch does not like some of the DP stuff
-        torch.serialization.add_safe_globals([Preprocessor])
-        torch.serialization.add_safe_globals([LanguageTokenizer])
-        torch.serialization.add_safe_globals([SequenceTokenizer])
 
         if num_gpus > 1:
             mp.spawn(train, nprocs=num_gpus, args=(num_gpus, config_file, args.checkpoint))
